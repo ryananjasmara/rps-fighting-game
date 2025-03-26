2bdd4686771d4e6d425071060ecb3926f4fdb41c
ryananjasmara <ryananjasmara1997@gmail.com>
2025-03-26 10:42:24 +0700

    fix: standalone output mode for next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Disable strict mode to prevent double socket connections in development
+  output: 'standalone',
 }
 
 module.exports = nextConfig
